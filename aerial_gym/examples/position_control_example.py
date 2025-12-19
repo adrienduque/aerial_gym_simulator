669e9a5ab17c20d126a3494e367f326ee1364110
adrienduque <adrienduquepro@gmail.com>
2025-12-19 16:51:02 +0900

    Back to original state

--- aerial_gym/examples/position_control_example.py
+++ aerial_gym/examples/position_control_example.py
@@ -28,5 +28,3 @@ if __name__ == "__main__":
             # actions[:, 3] = torch.pi * (torch.rand_like(actions[:, 3]) * 2 - 1)
             env_manager.reset()
         env_manager.step(actions=actions)
-        
-# And another one
